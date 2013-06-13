8c582940feea479edfd614f19843f9ff9b9d9900
benjamin <benjamin.martinez@lux.mx>
2013-06-13 11:29:34 -0500

    actualizacion de transacciones en panel de consulta

--- ui/src/main/groovy/mx/lux/pos/ui/model/InvTr.groovy
+++ ui/src/main/groovy/mx/lux/pos/ui/model/InvTr.groovy
@@ -272,9 +272,9 @@ class InvTr {
       postTrType = viewMode.getTrType()
     } else if ( viewMode.equals( InvTrViewMode.QUERY ) ) {
       postTrType = null
-      if ( qryDataset == null ) {
+      //if ( qryDataset == null ) {
         initDataset()
-      }
+      //}
     }
     else if ( viewMode.equals( InvTrViewMode.OUTBOUND ) ) {
         postTrType = viewMode.getTrType()
