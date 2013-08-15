e494a21e6d7f1718146e4aedace44f4dea34b58c
benjamin <benjamin.martinez@lux.mx>
2013-08-15 15:24:38 -0500

    actualizacion en correccion de terminales

--- ui/src/main/groovy/mx/lux/pos/ui/view/dialog/dailyclose/EditPaymentDialog.groovy
+++ ui/src/main/groovy/mx/lux/pos/ui/view/dialog/dailyclose/EditPaymentDialog.groovy
@@ -3,6 +3,7 @@ package mx.lux.pos.ui.view.dialog.dailyclose
 import groovy.swing.SwingBuilder
 import mx.lux.pos.service.business.Registry
 import mx.lux.pos.ui.controller.DailyCloseController
+import mx.lux.pos.ui.controller.OrderController
 import mx.lux.pos.ui.controller.PaymentController
 import mx.lux.pos.ui.model.Payment
 import mx.lux.pos.ui.model.Plan
@@ -13,6 +14,7 @@ import javax.swing.JLabel
 import javax.swing.JTextField
 import java.awt.event.ActionEvent
 import java.awt.event.ItemEvent
+import java.math.RoundingMode
 import java.text.NumberFormat
 import javax.swing.JButton
 import javax.swing.JComboBox
@@ -151,12 +153,23 @@ class EditPaymentDialog extends JDialog {
   private def doSave = { ActionEvent ev ->
     JButton source = ev.source as JButton
     source.enabled = false
+    Boolean validUSD = true
     Payment payment = new Payment()
     Boolean terminalUpdate = false
     if( Registry.isCardPaymentInDollars( tmpPayment.paymentTypeId ) && planUSD.text.isNumber() ){
-      tmpPayment.planId = planUSD.text
-      payment = DailyCloseController.updatePayment( tmpPayment )
-      terminalUpdate = DailyCloseController.updateTerminal( tmpPayment.date )
+      Double tipoCambio = OrderController.requestUsdRate()
+      BigDecimal pesosRec = tmpPayment.amount
+      BigDecimal dolaresRec = new BigDecimal(NumberFormat.getInstance().parse(planUSD.text.trim()))
+      BigDecimal dolaresCalc = pesosRec.divide(tipoCambio, 10, RoundingMode.HALF_EVEN)
+      Double diferencia = dolaresCalc.subtract(dolaresRec).doubleValue()
+      if( (diferencia < 1.0) && (diferencia > -1.0) ){
+        tmpPayment.planId = planUSD.text
+        payment = DailyCloseController.updatePayment( tmpPayment )
+        terminalUpdate = DailyCloseController.updateTerminal( tmpPayment.date )
+      } else {
+        validUSD = false
+        sb.optionPane().showMessageDialog( null, 'Cantidad de dolares incorrecta', 'Error', JOptionPane.ERROR_MESSAGE )
+      }
     } else if( !Registry.isCardPaymentInDollars( tmpPayment.paymentTypeId ) ){
       payment = DailyCloseController.updatePayment( tmpPayment )
       terminalUpdate = DailyCloseController.updateTerminal( tmpPayment.date )
@@ -165,7 +178,9 @@ class EditPaymentDialog extends JDialog {
       //sb.optionPane().showMessageDialog( null, 'Se ha aztualizado correctamente el Terminal y/o Plan del Pago', 'Ok', JOptionPane.INFORMATION_MESSAGE )
       dispose()
     } else {
-      sb.optionPane().showMessageDialog( null, 'Se ha producido un error al actualizar el Terminal y/o Plan del Pago', 'Error', JOptionPane.ERROR_MESSAGE )
+      if( validUSD ){
+        sb.optionPane().showMessageDialog( null, 'Se ha producido un error al actualizar el Terminal y/o Plan del Pago', 'Error', JOptionPane.ERROR_MESSAGE )
+      }
     }
     source.enabled = true
   }
