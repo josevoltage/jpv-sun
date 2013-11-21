644e31fba8e07751d62442c8c3d3e1c44941f931
benjamin <benjamin.martinez@lux.mx>
2013-11-21 15:27:15 -0600

    actualizacion pantalla espera

--- ui/src/main/groovy/mx/lux/pos/ui/view/dialog/WaitDialog.groovy
+++ ui/src/main/groovy/mx/lux/pos/ui/view/dialog/WaitDialog.groovy
@@ -42,6 +42,7 @@ class WaitDialog extends JDialog {
         modal: true,
         preferredSize: [ 280, 100 ],
         location: [ 200, 250 ],
+        undecorated: true,
     ) {
       panel() {
         borderLayout()
